ecc818ba49f2d98fc603e559a353a54d57d9af73
Gergo <113334790+gergogyulai@users.noreply.github.com>
2023-09-18 16:40:51 +0200

    Update tmdb-scraper.js

--- tmdb-scraper.js
+++ tmdb-scraper.js
@@ -17,66 +17,68 @@
 
 // Function to download an image and add it to the zip
 async function downloadAndAddToZip(url, zip) {
-    try {
-      const response = await fetch(url);
-      const blob = await response.blob();
-      
-      // Get the filename from the URL
-      const filename = url.substring(url.lastIndexOf('/') + 1);
-      
-      // Add the blob to the zip file
-      zip.file(filename, blob);
-      
-      console.log(`Downloaded and added to zip: ${filename}`);
-    } catch (error) {
-      console.error(`Error downloading: ${url}`, error);
-    }
+  try {
+    const response = await fetch(url);
+    const blob = await response.blob();
+
+    // Get the filename from the URL
+    const filename = url.substring(url.lastIndexOf('/') + 1);
+
+    // Add the blob to the zip file
+    zip.file(filename, blob);
+
+    console.log(`Downloaded and added to zip: ${filename}`);
+  } catch (error) {
+    console.error(`Error downloading: ${url}`, error);
   }
-  
-  // Function to create and download the zip file
-  function createAndDownloadZip(zip) {
-    zip.generateAsync({ type: 'blob' }).then(function (blob) {
-      const zipFilename = 'downloaded_images.zip';
-      const a = document.createElement('a');
-      a.href = URL.createObjectURL(blob);
-      a.download = zipFilename;
-      a.style.display = 'none';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      console.log('Zip file downloaded:', zipFilename);
-    });
+}
+
+// Function to create and download the zip file
+function createAndDownloadZip(zip, title) {
+  zip.generateAsync({ type: 'blob' }).then(function (blob) {
+    const zipFilename = `${title}.zip`; // Use the webpage title as the zip filename
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = zipFilename;
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    console.log('Zip file downloaded:', zipFilename);
+  });
+}
+
+// Function to scrape "View Original" links and download images
+function scrapeAndDownloadImages() {
+  // Check if JSZip is defined
+  if (typeof JSZip === 'undefined') {
+    console.error('JSZip library is not available.');
+    return;
   }
-  
-  // Function to scrape "View Original" links and download images
-  function scrapeAndDownloadImages() {
-    // Check if JSZip is defined
-    if (typeof JSZip === 'undefined') {
-      console.error('JSZip library is not available.');
-      return;
-    }
-  
-    // Find all <a> elements with alt text "View Original"
-    const linkElements = document.querySelectorAll('a[alt="View Original"]');
-  
-    // Create a new instance of JSZip
-    const zip = new JSZip();
-  
-    // Iterate through the found <a> elements and download their images
-    const downloadPromises = Array.from(linkElements).map((linkElement) => {
-      const imageUrl = `https://www.themoviedb.org${linkElement.getAttribute('href')}`;
-      return downloadAndAddToZip(imageUrl, zip);
-    });
-  
-    // Wait for all downloads to complete and then create and download the zip
-    Promise.all(downloadPromises).then(() => {
-      createAndDownloadZip(zip);
-    });
-  }
-  
-  // Inject JSZip library
-  var jsZipScript = document.createElement('script');
-  jsZipScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js';
-  jsZipScript.onload = scrapeAndDownloadImages; // Call the function once JSZip is loaded
-  document.head.appendChild(jsZipScript);
-  
+
+  // Get the webpage's title
+  const title = document.title;
+
+  // Find all <a> elements with alt text "View Original"
+  const linkElements = document.querySelectorAll('a[alt="View Original"]');
+
+  // Create a new instance of JSZip
+  const zip = new JSZip();
+
+  // Iterate through the found <a> elements and download their images
+  const downloadPromises = Array.from(linkElements).map((linkElement) => {
+    const imageUrl = `https://www.themoviedb.org${linkElement.getAttribute('href')}`;
+    return downloadAndAddToZip(imageUrl, zip);
+  });
+
+  // Wait for all downloads to complete and then create and download the zip
+  Promise.all(downloadPromises).then(() => {
+    createAndDownloadZip(zip, title); // Pass the webpage title as the zip filename
+  });
+}
+
+// Inject JSZip library
+var jsZipScript = document.createElement('script');
+jsZipScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js';
+jsZipScript.onload = scrapeAndDownloadImages; // Call the function once JSZip is loaded
+document.head.appendChild(jsZipScript);
